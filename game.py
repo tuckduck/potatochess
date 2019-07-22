95f24b5a4c4e29a3059ef40cf6378b32bd95fcfa
tuckduck <42545096+tuckduck@users.noreply.github.com>
2019-07-22 02:06:17 -0500

    Inventory/Equipment Menu implemented
    Modular Inventory/Equipment menu and functionality added. Can now equip an item from inventory, un-equip item to inventory and sprite will change accordingly. Hero Class is further built out to support inventory/equipment tracking and updating it's image.

--- game.py
+++ game.py
@@ -2,9 +2,6 @@ import sys, pygame
 import os
 import time
 from pygame.locals import *
-pygame.init()
-os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (1,31)
-size = width, height = 1400, 800
 
 def idleAnimation(tick, potat):
     if(tick // 4 % 4 == 3):
@@ -13,54 +10,171 @@ def idleAnimation(tick, potat):
         return pygame.transform.rotate(potat, tick // 4 % 2)
 def heroMove(presses):
     move = [0,0]
-    if(pressed[K_UP] == True):
+    if(pressed[K_w] or pressed[K_UP]):
         move[1] -=6
-    if(pressed[K_DOWN] == True):
+    if(pressed[K_s] or pressed[K_DOWN]):
         move[1] +=6
-    if(pressed[K_RIGHT] == True):
+    if(pressed[K_d] or pressed[K_RIGHT]):
         move[0] += 6
-    if(pressed[K_LEFT] == True):
+    if(pressed[K_a] or pressed[K_LEFT]):
         move[0] -= 6
     return move
+def inventoryMenu(background, hero):
+    invent_rects = {0: None, 1: None, 2: None, 3: None, 4: None, 5: None, 6: None, 7: None}
+    text = pygame.font.Font(None, 46)
+    text.set_bold(True)
+    hat = text.render("Hat", True, [0,0,0],[119,136,153])
+    hatRect = hat.get_rect()
+    hatRect.center = [1300,225]
+    body = text.render("Body", True, [0,0,0],[119,136,153])
+    bodyRect = body.get_rect()
+    bodyRect.center = [1300,425]
+    legs = text.render("Legs", True, [0,0,0],[119,136,153])
+    legsRect = legs.get_rect()
+    legsRect.center = [1300,625]
+    weapon = text.render("Weapon", True, [0,0,0],[119,136,153])
+    weaponRect = weapon.get_rect()
+    weaponRect.center = [1100,425]
+    equip_text = {0:[hat, hatRect],1:[body, bodyRect], 2:[legs, legsRect], 3:[weapon, weaponRect]}
+    equip_slots = drawInventoryMenu(background)
+    pygame.display.flip()
+    menu = pygame.display.get_surface().copy()
+    while(1):
+        screen.fill([0,0,0])
+        screen.blit(menu, [0,0])
+        for i in range(8):
+            if hero.inventory[i] is not None:
+                image = hero.inventory[i].image
+                rect = image.get_rect()
+                rect.center = [120 + i * 200, 550 + (i // 2) * 150]
+                invent_rects[i] = rect
+                screen.blit(image, rect)
+        for k in range(4):
+            if hero.equip[k] is not None:
+                item = hero.equip[k]
+                image = item.image
+                rect = image.get_rect()
+                rect.center = equip_slots[k].center
+                screen.blit(image, rect)
+            else:
+                screen.blit(equip_text[k][0], equip_text[k][1])
+        mouseState = pygame.mouse.get_pressed()
+        if mouseState[0]:
+            pos = pygame.mouse.get_pos()
+            for j in range(8):
+                if j < 4:
+                    if hero.equip[j]:
+                        if equip_slots[j].collidepoint(pos):
+                            hero.itemUnequip(hero.equip[j])
+                if invent_rects[j]:
+                    if invent_rects[j].collidepoint(pos):
+                        hero.itemEquip(hero.inventory[j])
+                        invent_rects.update({j:None})
+                        hero.inventory[j] = None
+        pygame.display.flip()
+        pygame.event.pump()
+        pressed = pygame.key.get_pressed()
+        
+        if pressed[K_q]:
+            break
+def drawInventoryMenu(snapshot):
+    screen.blit(snapshot,[0,0])
+    pygame.draw.rect(screen, [139,69,19],pygame.Rect(10,465,820,325))
+    pygame.draw.rect(screen, [101,67,33],pygame.Rect(20,475,800,305))
+
+    pygame.draw.rect(screen, [47, 79, 79],pygame.Rect(1215,340,170,170))
+    bodySlot = pygame.draw.rect(screen, [119,136,153],pygame.Rect(1225,350,150,150))
+
+    pygame.draw.rect(screen, [47, 79, 79],pygame.Rect(1215,540,170,170))
+    legsSlot = pygame.draw.rect(screen, [119,136,153],pygame.Rect(1225,550,150,150))
+
+    pygame.draw.rect(screen, [47, 79, 79],pygame.Rect(1015,340,170,170))
+    weaponSlot = pygame.draw.rect(screen, [119,136,153],pygame.Rect(1025,350,150,150))
+
+    pygame.draw.rect(screen, [47, 79, 79],pygame.Rect(1215,140,170,170))
+    hatSlot = pygame.draw.rect(screen, [119,136,153],pygame.Rect(1225,150,150,150))
+
+    return {0:hatSlot, 1:bodySlot, 2:legsSlot, 3:weaponSlot}
 class hero(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image_ = pygame.image.load("potat.png")
+        self.original = pygame.image.load("potat.png")
+        self.image_ = self.original
         self.image = None
         self.rect = self.image_.get_rect()
         self.rect.move_ip(620,610)
         self.hero_library = {0:"hatpotat.png"}
+        self.equip = {0:None, 1:None, 2:None, 3:None}
+        self.inventory = {0:None,1:None, 2:None, 3:None, 4:None, 5:None, 6:None, 7:None}
     def update(self, move):
         self.rect.move_ip(move[0],move[1])
-    def itemPickup(self, itemID):
-        self.image_ = pygame.image.load(self.hero_library.get(itemID))
+    def updateImage(self):
+        if self.equip[0] and not self.equip[3]:
+            width, height = 137, 250   #gross hardcode, needs revision
+        elif self.equip[0] and self.equip[3]:
+            width, height = 160, 250
+        else:
+            width, height = 137, 177
+        new_surf = pygame.Surface([width, height],pygame.SRCALPHA)
+        ori_rect = self.original.get_rect()
+        ori_rect.bottomright = new_surf.get_rect().bottomright
+        new_surf.blit(self.original, ori_rect)
+        if self.equip[0]:
+            new_surf.blit(self.equip[0].image, [0,0])
+        self.image_ = new_surf
+        new_rect = self.image_.get_rect()
+        new_rect.bottomright = self.rect.bottomright
+        self.rect = new_rect
+    def itemEquip(self, item):
+        self.equip.update({item.type:item})
+        self.updateImage()
+        #self.image_ = pygame.image.load(self.hero_library.get(itemID))
+    def itemUnequip(self,item):
+        self.equip.update({item.type:None})
+        self.placeInInventory(item)
+        self.updateImage()
+    def placeInInventory(self, item):
+        for i in range(8):
+            if self.inventory[i] is None:
+                self.inventory[i] = item
+                break
 
 class itemGenerator:
     def __init__(self):
-        self.spawns = [("starterhat.png", [900,500])]
+        self.spawns = [("starterhat.png", [900,500],0)]
     def spawnItem(self, itemID):
         spawn = self.spawns[itemID]
         self.image = pygame.image.load(spawn[0])
         self.rect = self.image.get_rect()
         self.rect.move_ip(spawn[1][0],spawn[1][1])
-        return item(self.image, self.rect,itemID)
+        itemObj = item(self.image, self.rect,itemID,spawn[2])
+                    
+        return itemObj
 
 class item(pygame.sprite.Sprite):
-    def __init__(self, image_, rect_,itemID):
+    def __init__(self, image_, rect_,itemID,itemType):
         pygame.sprite.Sprite.__init__(self)
+        self.type = itemType
         self.image = image_
         self.rect = rect_
         self.itemID = itemID
+pygame.init()
+os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (1,31)
+size = width, height = 1400, 800
 
 screen = pygame.display.set_mode(size)
+
 background = pygame.image.load("forestbackround.png")
 backrect = background.get_rect()
+
 potat = hero()
 heroSprite = pygame.sprite.GroupSingle()
 heroSprite.add(potat)
-item_spawns = itemGenerator()
+
+item_spawner = itemGenerator()
 current_spawns = pygame.sprite.Group()
-current_spawns.add(item_spawns.spawnItem(0))
+current_spawns.add(item_spawner.spawnItem(0))
+
 tick = 0 
 while 1:
     for event in pygame.event.get():
@@ -71,7 +185,9 @@ while 1:
     move = heroMove(pressed)
     item_pickups = pygame.sprite.spritecollide(potat, current_spawns, True)
     for item in item_pickups:
-        potat.itemPickup(item.itemID)
+        potat.placeInInventory(item)
+    if(pressed[K_e] == True):
+        inventoryMenu(pygame.display.get_surface(), potat)
     potat.update(move)
     screen.blit(background, backrect)
     current_spawns.draw(screen)
